b6c776df3b539da86bc9140fb61827cd69791fee
OmutnykhOleg596 <88890360+OmutnykhOleg596@users.noreply.github.com>
2021-11-05 18:22:36 +0500

    Create 1.3 Elementary cout.cpp

---
+++ Homework for C++/1.3 Elementary cout.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	const int constanta = 10;
+	int integer = 20;
+	cout << constanta << '\n';
+	cout << integer << '\n';
+	--integer;
+	cout << integer;
+	return 0;
+}
