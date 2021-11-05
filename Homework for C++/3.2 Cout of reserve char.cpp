e80f74fd4a73cd69079a98174ae2ca0bcd4cd359
OmutnykhOleg596 <88890360+OmutnykhOleg596@users.noreply.github.com>
2021-11-05 18:26:21 +0500

    Create 3.2 Cout of reserve char.cpp

---
+++ Homework for C++/3.2 Cout of reserve char.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	char a, b, c, d, e;
+	cin >> a >> b >> c >> d >> e;
+	cout << e << ' ' << d << ' ' << c << ' ' << b << ' ' << a;
+	return 0;
+}  
